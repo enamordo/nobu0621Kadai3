27fa52b575b31525ef8d9992812446f8b1819338
Atsunobu Yuge <yugechan666@gmail.com>
2024-07-23 00:15:43 +0900

    完成をコミット

--- Kadai3/ContentView.swift
+++ Kadai3/ContentView.swift
@@ -7,18 +7,69 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Kadai3View: View {
+    @State private var inputNumber1: String = ""
+    @State private var inputNumber2: String = ""
+    @State private var number1: Int = 0
+    @State private var number2: Int = 0
+    @State private var isOn1: Bool = false
+    @State private var isOn2: Bool = false
+    @State private var result: String = "0"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                TextField("", text: $inputNumber1)
+                    .frame(width: 100)
+                TextField("", text: $inputNumber2)
+                    .frame(width: 100)
+                Spacer()
+            }
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .keyboardType(.numberPad)
+            HStack {
+                Text("+")
+                    .frame(width: 25)
+                Toggle(isOn: $isOn1) {}
+                    .frame(width: 50)
+                    .tint(.indigo)
+                Text("-")
+                    .frame(width: 25)
+                Text("+")
+                    .frame(width: 25)
+                Toggle(isOn: $isOn2) {}
+                    .frame(width: 50)
+                    .tint(.orange)
+                Text("-")
+                    .frame(width: 25)
+                Spacer()
+            }
+            HStack {
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    number1 = Int(inputNumber1) ?? 0
+                    number1 = isOn1 ? -number1 : number1
+                    number2 = Int(inputNumber2) ?? 0
+                    number2 = isOn2 ? -number2 : number2
+                    result = String(number1 + number2)
+                }.frame(width: 180)
+                Spacer()
+            }
+            HStack {
+                Text(String(number1)).frame(width: 100)
+                Text("+")
+                Text(String(number2)).frame(width: 100)
+                Spacer()
+            }
+            HStack {
+                Text(result)
+                Spacer()
+            }
+            Spacer()
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    Kadai3View()
 }
--- Kadai3/Kadai3App.swift
+++ Kadai3/Kadai3App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Kadai3App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Kadai3View()
         }
     }
 }
