16e0c23cfa53217226345a52764e06fb74b08c23
Atsunobu Yuge <yugechan666@gmail.com>
2024-07-22 22:37:40 +0900

    Initial Commit

---
+++ Kadai3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Kadai3
+//
+//  Created by nobu0621 on 2024/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Kadai3/Kadai3App.swift
@@ -0,0 +1,17 @@
+//
+//  Kadai3App.swift
+//  Kadai3
+//
+//  Created by nobu0621 on 2024/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Kadai3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
